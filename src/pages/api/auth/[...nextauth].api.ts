4ee77a847b4f111b7c2463c43b9fd9d2b6a9e842
Vinicius - Notebook Luana <luanamrclaraujo@gmail.com>
2023-03-17 21:19:33 -0300

    config next-auth Provider Google - Funcional

---
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -0,0 +1,39 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
+
+export function buildNextAuthOptions(
+  req: NextApiRequest,
+  res: NextApiResponse
+): NextAuthOptions {
+  return {
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+        authorization: {
+          params: {
+            prompt: "consent",
+            access_type: "offline",
+            response_type: "code",
+            scope:
+              "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email ",
+          },
+        },
+        // // Restorna os dados do usuarios
+        // profile(profile: GoogleProfile) {
+        //   return {
+        //     id: profile.sub,
+        //     name: profile.name,
+        //     email: profile.email,
+        //     avatar_url: profile.picture,
+        //   };
+        // },
+      }),
+    ],
+  };
+}
+
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return await NextAuth(req, res, buildNextAuthOptions(req, res));
+}
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,26 +1,39 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import { Button } from '@foreverdark/react/dist'
-import LogoGoogle from "../../assets/logo-google.svg"
-import bgHome from "../../assets/books/bg-home.png"
-import Logo from "../../assets/books/logo.svg"
-import JetLogin from "../../assets/books/rocketLaunch.svg"
-import { FcGoogle } from 'react-icons/fc';
-import { FaGithub } from 'react-icons/fa';
-import { ButtonStyles, ContainerHome, ConteinerLogin, ContentBg, ContentButton, ContentHome, ContentImage, ContentLogin, ImageBackground, ImageLogo } from './styles'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { Button } from "@foreverdark/react/dist";
+import LogoGoogle from "../../assets/logo-google.svg";
+import bgHome from "../../assets/books/bg-home.png";
+import Logo from "../../assets/books/logo.svg";
+import JetLogin from "../../assets/books/rocketLaunch.svg";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import {
+  ButtonStyles,
+  ContainerHome,
+  ConteinerLogin,
+  ContentBg,
+  ContentButton,
+  ContentHome,
+  ContentImage,
+  ContentLogin,
+  ImageBackground,
+  ImageLogo,
+} from "./styles";
+import { signIn } from "next-auth/react";
 
 export default function Home() {
+  async function handleConnectGoogle() {
+    await signIn("google");
+  }
   return (
     <ContainerHome>
       <ContentHome>
         <ContentBg>
           <ContentImage>
-
             <ImageBackground src={bgHome} alt="backgroud" />
             <ImageLogo src={Logo} alt="backgroud" />
-          
           </ContentImage>
         </ContentBg>
 
@@ -32,16 +45,16 @@ export default function Home() {
             </header>
 
             <ContentButton>
-              <ButtonStyles>
-                <FcGoogle size={32}/>
+              <ButtonStyles onClick={handleConnectGoogle}>
+                <FcGoogle size={32} />
                 Entrar com Google
               </ButtonStyles>
-    
+
               <ButtonStyles>
                 <FaGithub />
                 Entrar com Github
               </ButtonStyles>
-    
+
               <ButtonStyles>
                 <Image src={JetLogin} alt="" />
                 Acessar como visitante
@@ -51,5 +64,5 @@ export default function Home() {
         </ConteinerLogin>
       </ContentHome>
     </ContainerHome>
-  )
+  );
 }

ae735c8f6aa4e4fa7e9f5b8b6019592182b4ae86
Vinicius - Notebook Luana <luanamrclaraujo@gmail.com>
2023-03-27 18:30:43 -0300

    commit - Luana

--- src/pages/profile/index.page.tsx
+++ src/pages/profile/index.page.tsx
@@ -1,9 +1,20 @@
 import { MagnifyingGlass, User } from "@phosphor-icons/react";
 import { RatingStarts } from "../components/ratingStars";
 import { SideBar } from "../components/sideBar";
-import { AssessmentsTheUsersContainer, AssessmentsTheUsersContent, ContainerProfile, ContentProfile, Input, InputConteiner, MainContainer, MainContent, ProfileConteiner } from "./styles";
+import {
+  AssessmentsTheUsersContainer,
+  AssessmentsTheUsersContent,
+  ContainerProfile,
+  ContentCite,
+  ContentProfile,
+  Input,
+  InputConteiner,
+  MainContainer,
+  MainContent,
+  ProfileConteiner,
+} from "./styles";
 import Image from "next/image";
-import HabitosDeDesenvolvedores from "../../../assets/books/14-habitos-de-desenvolvedores-altamente-produtivos.png"
+import HabitosDeDesenvolvedores from "../../../assets/books/14-habitos-de-desenvolvedores-altamente-produtivos.png";
 
 export default function Profile() {
   return (
@@ -21,37 +32,80 @@ export default function Profile() {
             <Input placeholder="Buscar livro avaliado" />
 
             <MagnifyingGlass />
-
           </InputConteiner>
-          
+
           <AssessmentsTheUsersContainer>
             <AssessmentsTheUsersContent>
               <MainContainer>
                 <MainContent>
+                  <time>Há 2 dias</time>
+                  <section>
+                    <header>
+                      <Image
+                        src={HabitosDeDesenvolvedores}
+                        width={108}
+                        height={152}
+                        alt=""
+                      />
+
+                      <ContentCite>
+                        <cite>
+                          <strong>14 Habitos desenvolvedores</strong>
+                          <span>Zeno Rocha</span>
+                        </cite>
+                        <RatingStarts size="1rem" />
+                      </ContentCite>
+                    </header>
+
+                    <main>
+                      <p>
+                        Semper et sapien proin vitae nisi. Feugiat neque integer
+                        donec et aenean posuere amet ultrices. Cras fermentum id
+                        pulvinar varius leo a in. Amet libero pharetra nunc
+                        elementum fringilla velit ipsum. Sed vulputate massa
+                        velit nibh... ver mais
+                      </p>
+                    </main>
+                  </section>
+                </MainContent>
+              </MainContainer>
+              <MainContent>
+                <time>Há 2 dias</time>
+                <section>
                   <header>
-                    <Image src={HabitosDeDesenvolvedores} width={108} height={152} alt="" />
+                    <Image
+                      src={HabitosDeDesenvolvedores}
+                      width={108}
+                      height={152}
+                      alt=""
+                    />
 
-                    <cite>
-                      <strong>14 Habitos desenvolvedores</strong>
-                      <span>Zeno Rocha</span>
-                    </cite>
-
-                    <RatingStarts size="1rem" />
+                    <ContentCite>
+                      <cite>
+                        <strong>14 Habitos desenvolvedores</strong>
+                        <span>Zeno Rocha</span>
+                      </cite>
+                      <RatingStarts size="1rem" />
+                    </ContentCite>
                   </header>
 
                   <main>
-                    <p>Semper et sapien proin vitae nisi. Feugiat neque integer donec et aenean posuere amet ultrices. Cras fermentum id pulvinar varius leo a in. Amet libero pharetra nunc elementum fringilla velit ipsum. Sed vulputate massa velit nibh... ver mais</p>
+                    <p>
+                      Semper et sapien proin vitae nisi. Feugiat neque integer
+                      donec et aenean posuere amet ultrices. Cras fermentum id
+                      pulvinar varius leo a in. Amet libero pharetra nunc
+                      elementum fringilla velit ipsum. Sed vulputate massa velit
+                      nibh... ver mais
+                    </p>
                   </main>
-                </MainContent>
-              </MainContainer>
+                </section>
+              </MainContent>
             </AssessmentsTheUsersContent>
-
           </AssessmentsTheUsersContainer>
         </ProfileConteiner>
-
 
         <h1>Outro</h1>
       </ContentProfile>
     </ContainerProfile>
-  )
+  );
 }
--- src/pages/profile/styles.ts
+++ src/pages/profile/styles.ts
@@ -1,4 +1,4 @@
-import {styled} from "../../styles/stitches.config" 
+import { styled } from "../../styles/stitches.config";
 
 export const ContainerProfile = styled("div", {
   maxWidth: "90rem",
@@ -9,7 +9,7 @@ export const ContainerProfile = styled("div", {
   "@media (max-width: 768px)": {
     // padding: "0"
   },
-})
+});
 
 export const ContentProfile = styled("div", {
   display: "grid",
@@ -18,17 +18,23 @@ export const ContentProfile = styled("div", {
   width: "100%",
 
   "@media (max-width: 768px)": {
-    all: "unset"
-  }
-})
+    all: "unset",
+  },
+});
 
 export const ProfileConteiner = styled("div", {
   marginTop: "4.5rem",
   header: {
     display: "flex",
-    gap: "0.75rem"
-  }
-})
+    gap: "0.75rem",
+  },
+
+  "@media(max-width: 768px)": {
+    header: {
+      padding: "0.5rem",
+    },
+  },
+});
 
 export const InputConteiner = styled("div", {
   marginTop: "2.5rem",
@@ -47,45 +53,102 @@ export const InputConteiner = styled("div", {
     fontSize: "0.875rem",
   },
 
-  '&:has(input:focus)': {
-    borderColor: '$green200',
+  "&:has(input:focus)": {
+    borderColor: "$green200",
   },
 
-  '&:has(input:disabled)': {
+  "&:has(input:disabled)": {
     opacity: 0.5,
-    cursor: 'not-allowed',
+    cursor: "not-allowed",
   },
 
   svg: {
     width: "1.25rem",
     color: "$gray500",
-    height: "1.25rem"
-  }
-})
+    height: "1.25rem",
+  },
+
+  "@media(max-width: 768px)": {
+    margin: "0.5rem",
+  },
+});
 
 export const Input = styled("input", {
   all: "unset",
   width: "100%",
   padding: "0.875rem 0.5rem 0.875rem 1.25rem",
-
-
-})
+});
 
 export const AssessmentsTheUsersContainer = styled("div", {
   display: "flex",
   width: "100%",
-})
 
-export const AssessmentsTheUsersContent = styled("div", {
+  "@media(max-width: 768px)": {
+    padding: "0.5rem",
+  },
+});
 
-})
+export const AssessmentsTheUsersContent = styled("div", {});
 
-export const MainContainer = styled("div", {
-
-})
+export const MainContainer = styled("div", {});
 
 export const MainContent = styled("div", {
+  time: {
+    fontSize: "0.875rem",
+    color: "$gray300",
+  },
 
-})
+  section: {
+    padding: "1.5rem",
+    background: "$gray700",
+    borderRadius: "$md",
+    marginTop: "0.5rem",
+    marginBottom: "1.5rem",
 
+    header: {
+      marginBottom: "1.5rem",
+      display: "flex",
+      width: "100%",
 
+      img: {
+        width: "6.125rem",
+        height: "8.375rem",
+      },
+
+      cite: {
+        display: "flex",
+        flexDirection: "column",
+      },
+    },
+
+    main: {
+      color: "$gray300",
+      fontSize: "0.875rem",
+      lineHeight: "$base",
+    },
+  },
+});
+
+export const ContentCite = styled("div", {
+  width: "100%",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+
+  cite: {
+    fontStyle: "normal",
+
+    strong: {
+      fontSize: "1.125rem",
+    },
+
+    span: {
+      color: "$gray400",
+      fontSize: "0.875rem",
+    },
+  },
+
+  svg: {
+    color: "$purple100",
+  },
+});

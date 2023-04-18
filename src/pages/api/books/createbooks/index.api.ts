10a8e6b3bb13e44fe9676f994b0dcbef413a8ea0
Vinicius Ferreira <vinicius.fg05@gmail.com>
2023-04-18 10:12:19 -0300

    comentando as rotas da api

--- src/pages/api/books/createbooks/index.api.ts
+++ src/pages/api/books/createbooks/index.api.ts
@@ -1,54 +1,54 @@
-import { prisma } from "@/lib/prisma";
-import { NextApiRequest, NextApiResponse } from "next";
-import datas from "../../../../../data.json"
+// import { prisma } from "@/lib/prisma";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import datas from "../../../../../data.json"
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method !== "POST") {
-    return res.status(405).end();
-  }
-
-
-  const { name, username } = req.body;
-  for await(let data of datas) {
-    const categorie = await prisma.category.findMany({
-      where: {
-        name: {
-          in: data.category,
-        },
-      },
-      select: {
-        id: true,
-      },
-    });
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   if (req.method !== "POST") {
+//     return res.status(405).end();
+//   }
 
 
-    const bookExists = await prisma.book.findFirst({
-      where: {
-        cover_url: data.image
-      }
-    })
+//   const { name, username } = req.body;
+//   for await(let data of datas) {
+//     const categorie = await prisma.category.findMany({
+//       where: {
+//         name: {
+//           in: data.category,
+//         },
+//       },
+//       select: {
+//         id: true,
+//       },
+//     });
 
-    console.log(data.numberPages)
 
-    if (!bookExists) {
+//     const bookExists = await prisma.book.findFirst({
+//       where: {
+//         cover_url: data.image
+//       }
+//     })
 
-      await prisma.book.create({
-        data: {
-          name: data.title,
-          cover_url: data.image,
-          total_pages: Number(data.numberPages),
-          author: data.author,
-          summary: data.descriptionBooks,
-          categories: {
-            connect: categorie
-          }
-        }
-      })
-    }
-  }
-  return res.status(201).end();
-}
+//     console.log(data.numberPages)
+
+//     if (!bookExists) {
+
+//       await prisma.book.create({
+//         data: {
+//           name: data.title,
+//           cover_url: data.image,
+//           total_pages: Number(data.numberPages),
+//           author: data.author,
+//           summary: data.descriptionBooks,
+//           categories: {
+//             connect: categorie
+//           }
+//         }
+//       })
+//     }
+//   }
+//   return res.status(201).end();
+// }
 
--- src/pages/api/books/rating/index.api.ts
+++ src/pages/api/books/rating/index.api.ts
@@ -1,37 +1,37 @@
-import { prisma } from "@/lib/prisma";
-import { NextApiRequest, NextApiResponse } from "next";
-import datas from "../../../../../data.json";
+// import { prisma } from "@/lib/prisma";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import datas from "../../../../../data.json";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method !== "POST") {
-    return res.status(405).end();
-  }
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   if (req.method !== "POST") {
+//     return res.status(405).end();
+//   }
 
-  const { createRating: {description, rate, book_id, user_id} } = req.body;
+//   const { createRating: {description, rate, book_id, user_id} } = req.body;
 
-  const ifUserExists = await prisma.user.findUnique({
-    where: {
-      id: user_id,
-    }
-  })
+//   const ifUserExists = await prisma.user.findUnique({
+//     where: {
+//       id: user_id,
+//     }
+//   })
 
-  if(ifUserExists) {
-    const createRatingg = await prisma.rating.create({
-      data: {
-        rate,
-        description,
-        book_id,
-        user_id
-      }
-    })
-  } else {
-    return res.status(404).end();
-  }
+//   if(ifUserExists) {
+//     const createRatingg = await prisma.rating.create({
+//       data: {
+//         rate,
+//         description,
+//         book_id,
+//         user_id
+//       }
+//     })
+//   } else {
+//     return res.status(404).end();
+//   }
 
 
   
-  return res.status(201).end();
-}
+//   return res.status(201).end();
+// }
